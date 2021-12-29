eacdf3c834365e1b80d0b5b6437a1a6ddf37c121
Joshe Neira <josemak10@gmail.com>
2021-12-29 16:03:36 -0500

    fix auto height image

--- src/router/RouterPage.js
+++ src/router/RouterPage.js
@@ -22,7 +22,7 @@ export const RouterPage = () => {
                     src={ img_header }
                     alt='header'
                     width="100%"
-                    height="18%"
+                    height="auto"
                 />
                 <Switch>
                     <Route exact path="/pagos-en-linea" component={ContainerQueryData} />
@@ -35,7 +35,7 @@ export const RouterPage = () => {
                     src={ img_footer }
                     alt='header'
                     width="100%"
-                    height="7%"
+                    height="auto"
                 />
             </div>
         </Router>
